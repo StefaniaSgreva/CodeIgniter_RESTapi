6faebf459530977143a122b449f19388fffb7253
Stefania Sgreva <stefaniasgreva@gmail.com>
2023-05-29 16:47:06 +0200

    Blog create

--- app/Controllers/Blog.php
+++ app/Controllers/Blog.php
@@ -13,4 +13,27 @@ class Blog extends ResourceController
         $posts = $this->model->findAll();
         return $this->respond($posts);
     }
+
+    public function create(){
+        helper(['form']);
+
+        $rules = [
+            'title' => 'required|min_length[6]',
+            'description' => 'required'
+        ];
+
+        if(!$this->validate($rules)){
+            return $this->fail($this->validator->getErrors());
+        }else{
+            $data = [
+                'post_title' => $this->request->getVar('title'),
+                'post_description' => $this->request->getVar('description'),
+            ];
+
+            $post_id = $this->model->insert($data);
+            $data['post_id'] = $post_id;
+
+            return $this->respondCreated($data);//generate http code 201
+        }
+    }
 }
